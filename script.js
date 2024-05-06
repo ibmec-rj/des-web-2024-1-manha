c870821c925c42b65d5825b092483eb77368061d
EDUARDO FREITAS MANGELI DE BRITO <06960720761@professores.ibmec.edu.br>
2024-05-06 11:15:57 -0300

    card único pronto

--- script.js
+++ script.js
@@ -34,8 +34,10 @@ const conteudo = document.getElementById("conteúdo");
 
 conteudo.innerHTML = `
     <div class='card'>
-        <img src='${atleta.imagem}' alt='Foto de ${atleta.nome}'>
-        <p>${atleta.posicao}</p>
+        <div class='img-cont'>
+            <img src='${atleta.imagem}' alt='Foto de ${atleta.nome}'>
+        </div>
+        <p class='posicao'>${atleta.posicao}</p>
         <p class='nome'>${atleta.nome}</p>
         <p class='descricao'>${atleta.descricao}</p>
         <p class='nascimento'>${atleta.nascimento}</p>
