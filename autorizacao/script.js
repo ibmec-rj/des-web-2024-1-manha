bf682b0647481ea9aa23c259860b4420d727ba99
EDUARDO FREITAS MANGELI DE BRITO <06960720761@professores.ibmec.edu.br>
2024-05-27 09:11:26 -0300

    finalizando hash

--- autorizacao/script.js
+++ autorizacao/script.js
@@ -1,10 +1,13 @@
-const alvo = 'e7d80ffeefa212b7c5c55700e4f7193e';
+const alvo = '4dd2f3f0570fde68c11b25976f8a7addaa2fbdec2a3ad8880087059b1f49701fb0242662b8ec77502e165fd794d2ef116659d52b5527effbedecd6209957698e';
+const sal = 'um_sal';
 
 document.getElementById('btn_login').onclick = () => {
     const entrada = document.getElementById('senha').value;
     const mensagem = document.getElementById('mensagem');
-    if (hex_md5(entrada) === alvo){
+    if (hex_sha512(entrada + sal) === alvo){
         mensagem.innerHTML = "<h2>Senha correta.</h2>";
+        sessionStorage.setItem('logado', 1);
+        window.location.href = 'nova.html';
     } else {
         mensagem.innerHTML = "<h2>Senha errada!!!</h2>";
     }
